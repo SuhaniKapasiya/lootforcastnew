3142e9563bce4dcf1d693c88d419870835233944
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2024-12-05 00:03:53 +0530

    added shimmer effect UI

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,10 +1,30 @@
-const Shimmer =()=>{
-    return(
-        <div className="bg-red-500 w-4/6">
-             <h1>card</h1>
-        </div>
-    )
-}
+import React from "react";
 
+const Shimmer = () => {
+  return (
+    <div className="flex flex-wrap gap-4 p-4 h-96">
+      {Array(12) // Adjust the number of shimmer cards
+        .fill("")
+        .map((_, index) => (
+          <div
+            key={index}
+            className="w-48 p-4 bg-gray-200 rounded-lg animate-pulse"
+          >
+            {/* Image Placeholder */}
+            <div className="h-28 bg-gray-300 rounded-lg"></div>
 
-export default Shimmer
+            {/* Title Placeholder */}
+            <div className="mt-4 h-4 bg-gray-300 rounded"></div>
+
+            {/* Subtitle Placeholder */}
+            <div className="mt-2 h-3 bg-gray-300 rounded w-3/4"></div>
+
+            {/* Price Placeholder */}
+            <div className="mt-4 h-4 bg-gray-300 rounded w-1/2"></div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Shimmer;
